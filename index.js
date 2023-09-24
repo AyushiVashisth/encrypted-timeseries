13aa0a92ef19dae149570f79157dff4117e3737b
Ayushi Vashisth <ayushivashisth22@gmail.com>
2023-09-25 00:55:45 +0530

    update-frontend

--- index.js
+++ index.js
@@ -1,113 +1,140 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const crypto = require("crypto");
-const mongoose = require("mongoose");
-const EmitterModel = require("./models/emitter.model");
-const connection = require("./config/db");
-const cors = require("cors");
-require("dotenv").config();
-const fs = require("fs");
-const timesSeriesModel = require("./models/timesSeries.model");
+// Import required modules and libraries
+const express = require("express"); // Express.js for creating the web server
+const http = require("http"); // HTTP module for creating an HTTP server
+const { Server } = require("socket.io"); // Socket.io for WebSocket communication
+const crypto = require("crypto"); // Node.js crypto module for encryption and hashing
+const connection = require("./config/db"); // Database connection module
+const cors = require("cors"); // CORS middleware for handling cross-origin requests
+require("dotenv").config(); // Load environment variables
+const fs = require("fs"); // File system module for file operations
+const TimeSeriesModel = require("./models/timesSeries.model"); // Database model for Time Series data
+const data = JSON.parse(fs.readFileSync("data.json")); // Read data from a JSON file
 
-const data = JSON.parse(fs.readFileSync("data.json"));
-
+// Create an Express application
 const app = express();
+
+// Create an HTTP server using Express
 const server = http.createServer(app);
+
+// Create a Socket.io server attached to the HTTP server
 const io = new Server(server);
+
+// Enable CORS for the Express app
 app.use(cors());
 
-// // Emitter service
-function generateRandomString(length) {
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    result += charset.charAt(randomIndex);
-  }
-  return result;
-}
-
+// Function to generate a secret key based on name, origin, and destination
 function generateSecretKey(name, origin, destination) {
   const data = `${name}${origin}${destination}`;
   return crypto.createHash("sha256").update(data).digest("hex");
 }
 
+// Function to encrypt a message using AES-256 encryption
 function encryptMessage(message, passkey) {
   const cipher = crypto.createCipher("aes-256-ctr", passkey);
   let encryptedMessage = cipher.update(message, "utf8", "hex");
   encryptedMessage += cipher.final("hex");
+  // Note: decryptMessage is called here, but its return value is not used or stored
   decryptMessage(encryptedMessage, passkey);
-  // console.log("encrypted message", encryptedMessage);
   return encryptedMessage;
 }
 
+// Function to decrypt a message using AES-256 decryption
 function decryptMessage(encryptedMessage, passkey) {
   const decipher = crypto.createDecipher("aes-256-ctr", passkey);
   let decryptedMessage = decipher.update(encryptedMessage, "hex", "utf8");
   decryptedMessage += decipher.final("utf8");
-  // console.log("decryptedMessage", decryptedMessage);
-  //   return decryptedMessage;
+  return decryptedMessage;
 }
 
+// Function to generate and emit a data stream to connected clients via WebSocket
 function generateAndEmitDataStream(socket) {
-  const dataStream = [];
-  //   const dataCount = Math.floor(Math.random() * 451) + 49;
-  const dataCount = 11;
+  const name = data.names[Math.floor(Math.random() * data.names.length)];
+  const origin = data.cities[Math.floor(Math.random() * data.cities.length)];
+  const destination =
+    data.cities[Math.floor(Math.random() * data.cities.length)];
 
-  for (let i = 0; i < dataCount; i++) {
-    const name = data.names[Math.floor(Math.random() * data.names.length)];
-    const origin = data.cities[Math.floor(Math.random() * data.cities.length)];
-    const destination =
-      data.cities[Math.floor(Math.random() * data.cities.length)];
-
-    const secretKey = generateSecretKey(name, origin, destination);
-    const originalMessage = { name, origin, destination };
-    const encryptedMessage = encryptMessage(
-      JSON.stringify(originalMessage),
-      secretKey
-    );
-
-    dataStream.push({
-      name,
-      origin,
-      destination,
-      secret_key: secretKey,
-      encryptedMessage
-    });
-  }
-
-  // Save data to MongoDB with timestamp
-  const timestamp = new Date();
-  const newData = new timesSeriesModel({
-    timestamp,
-    data: dataStream
-  });
-  newData.save();
-
-  const dataStreamString = dataStream
-    .map((item) => item.encryptedMessage)
-    .join("|");
-  socket.emit("dataStream", dataStreamString);
+  const secretKey = generateSecretKey(name, origin, destination);
+  const originalMessage = {
+    name,
+    origin,
+    destination,
+    secretKey,
+    timeStamp: Date.now(),
+    meta: 5
+  };
+  const encryptedMessage = encryptMessage(
+    JSON.stringify(originalMessage),
+    secretKey
+  );
+  socket.emit("encrypted", { encryptedMessage, secretKey });
 }
 
+// Function to validate received data
+function isDataValid(userValue, key) {
+  const currentTime = Date.now();
+  if (
+    Object.keys(userValue).length === userValue.meta + 1 &&
+    userValue.secretKey === key
+  ) {
+    const timeDifference = currentTime - userValue.timeStamp;
+    if (timeDifference <= 60000) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Event listener for WebSocket connections
 io.on("connection", (socket) => {
-  // socket.emit("msg", "connected");
-  // console.log("Client connected");
+  console.log("Client connected");
+
+  // Periodically generate and emit data streams to connected clients
   setInterval(() => {
     generateAndEmitDataStream(socket);
-  }, 10000); // Emit data stream every 10 seconds
+  }, 10000);
+
+  // Event listener for "listen_request" messages from clients
+  socket.on("listen_request", (receivedData) => {
+    console.log("Received data:", receivedData);
+    const decryptedData = JSON.parse(
+      decryptMessage(receivedData.encryptedMessage, receivedData.secretKey)
+    );
+
+    // Validate the received data and save it to the database
+    if (isDataValid(decryptedData, receivedData.secretKey)) {
+      const { name, origin, destination, secretKey, timeStamp } = decryptedData;
+      const timeSeries = new TimeSeriesModel({
+        name,
+        origin,
+        destination,
+        secretKey,
+        timeStamp
+      });
+      timeSeries.save();
+
+      // Emit the decoded data back to the client
+      socket.emit("decoded", {
+        name,
+        origin,
+        destination,
+        secretKey,
+        timeStamp
+      });
+    }
+  });
 });
 
-// Start the server
+// Define the port number for the server, using an environment variable or defaulting to 5000
 const PORT = process.env.PORT || 5000;
+
+// Start the server and establish a database connection
 server.listen(PORT, async () => {
   try {
     await connection;
-    console.log("Connected to Darabase");
+    console.log("Connected to Database");
   } catch (e) {
     console.log("Error connecting to database");
-    resizeBy.status(404).send(e.message);
+    res.status(404).send(e.message);
   }
   console.log(`Server is running on port ${PORT}`);
 });
--- models/timesSeries.model.js
+++ models/timesSeries.model.js
@@ -1,16 +1,14 @@
-const mongoose = require('mongoose');
+// Import the Mongoose library for working with MongoDB
+const mongoose = require("mongoose");
 
+// Define a schema for the "TimeSeries" data
 const timeSeriesSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  data: [
-    {
-      name: String,
-      origin: String,
-      destination: String,
-      secret_key: String,
-      encryptedMessage: String,
-    },
-  ],
+  name: String, // Store a name as a string
+  origin: String, // Store an origin as a string
+  destination: String, // Store a destination as a string
+  secret_key: String, // Store a secret key as a string
+  timeStamp: { type: Date, default: Date.now } // Store a timestamp as a Date, defaulting to the current date and time
 });
 
-module.exports = mongoose.model('TimeSeries', timeSeriesSchema);
+// Create a Mongoose model named "TimeSeries" based on the defined schema
+module.exports = mongoose.model("TimeSeries", timeSeriesSchema);
